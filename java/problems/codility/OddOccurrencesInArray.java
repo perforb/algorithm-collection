851c170320cce54508492364401de49c9f91dc3c
perforb <dev.perfumed.garden@gmail.com>
2020-10-15 20:26:13 +0900

    Polish

--- java/problems/codility/OddOccurrencesInArray.java
+++ java/problems/codility/OddOccurrencesInArray.java
@@ -17,7 +17,7 @@ public class OddOccurrencesInArray {
 
     public int solution(int[] A) {
         Arrays.sort(A);
-        for (int i = 0; i < A.length - 1; i = i + 2) {
+        for (int i = 0; i < A.length - 1; i += 2) {
             if (A[i] != A[i + 1]) {
                 return A[i];
             }
